80022244cddb80daeba5a9871704f5dd537d1023
nicesh0571 <137755197+nicesh0571@users.noreply.github.com>
2025-05-26 03:37:26 +0900

    Delete Wound_Classify directory

--- Wound_Classify/Classify/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.camera_page, name='camera'),  # camera.html → index.html 사용 시 바꿔도 OK
-    path('video_feed/', views.video_feed, name='video_feed'),
-    path('label_info/', views.get_label_info, name='label_info'),
-]
